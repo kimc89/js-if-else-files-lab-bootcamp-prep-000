3325c762457c84986af5769b2e5551c076a9489b
Kimberly Cushing <kimcushing89@gmail.com>
2017-11-20 22:04:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ if (name === "Susan") {
   console.log("Expected the name to be Susan")
 }
 var height = "74"
+if (height === "74") {
+  console.log("The height is correct")
+}
 
 
 // Don't worry about this
